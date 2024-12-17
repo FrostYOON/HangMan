f0469499baeca5a04d29192f62f8ebaec00aec3e
FrostYOON <yys1423@naver.com>
2024-12-17 17:39:53 +0900

    second time develop

--- system.js
+++ system.js
@@ -1,4 +1,5 @@
 import { makeElementVisible, makeElementInvisible } from './utils.js';
+import Game from "./game.js";
 
 class System {
   // 남은 시도 횟수
@@ -20,11 +21,13 @@ class System {
 
     // 시작 화면 초기화
     this.remainingAttempts.innerText = this.#count;
-    this.mainWordButton.addEventListener('click', () => {
-      this.mainWordTitle.innerText = '행맨 게임';
-      this.mainWordButton.innerText = '시작하기';
-      makeElementVisible(this.mainWord);
-    });
+
+    makeElementVisible(this.mainWord);
+    if (this.mainWordButton.innerText === '시작하기') {
+      this.mainWordButton.addEventListener('click', () => this.#start());
+    }else if (this.mainWordButton.innerText === '다시 시작') {
+      this.mainWordButton.addEventListener('click', () => this.#restart());
+    }
   }
 
   // 남은 시도 횟수 감소
@@ -38,22 +41,20 @@ class System {
     this.#decreaseCount();
     const result = await new Game();
 
-    if (result === 'win') {
+    if (result) {
       this.mainWordTitle.innerText = '성공했습니다!';
       this.mainWordButton.innerText = '처음부터 다시하기';
       makeElementVisible(this.mainWord);
-    } else if (result === 'lose' && this.#count > 0) {
-      this.mainWordTitle.innerText = '실패했습니다!';
-      this.mainWordButton.innerText = '다시 시작';
-      makeElementVisible(this.mainWord);
-    } else if (result === 'lose' && this.#count === 0) {
-      this.mainWordTitle.innerText = '기회를 모두 사용했습니다';
-      this.mainWordButton.innerText = '처음부터 다시하기';
-      makeElementVisible(this.mainWord);
     } else {
-      this.mainWordTitle.innerText = '행맨 게임';
-      this.mainWordButton.innerText = '시작하기';
-      makeElementVisible(this.mainWord);
+      if (this.#count > 0) {
+        this.mainWordTitle.innerText = '실패했습니다!';
+        this.mainWordButton.innerText = '다시 시작';
+        makeElementVisible(this.mainWord);
+      } else {
+        this.mainWordTitle.innerText = '기회를 모두 사용했습니다';
+        this.mainWordButton.innerText = '처음부터 다시하기';
+        makeElementVisible(this.mainWord);
+      }
     }
   }
 
@@ -70,11 +71,11 @@ class System {
   }
 
   // 게임 초기화
-  #cleanup() {
-    this.#count = 7;
-    this.remainingAttempts.innerText = this.#count;
-    this.mainWordTitle.innerText = '행맨 게임';
-    this.mainWordButton.innerText = '시작하기';
+  cleanup() {
+    this.remainingAttempts.innerText = 7;
+    this.mainWordButton.removeEventListener('click', () => this.#start());
+    this.mainWordButton.removeEventListener('click', () => this.#restart());
+    makeElementInvisible(this.answerWord);
     makeElementVisible(this.mainWord);
   }
 }
